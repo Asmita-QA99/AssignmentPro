0fe0d37d8f88bb1aa5b844c2ee7ac23118d72795
Asmita-QA99 <asmita.bramhankar99@gmail.com>
2025-06-25 20:53:49 +0530

    Create SwapValueUsingThirdV.java
    added SwapUsingThirdV Class

---
+++ src/SwapPackage/SwapValueUsingThirdV.java
@@ -0,0 +1,27 @@
+package SwapPackage;
+
+public class SwapValueUsingThirdV {
+    public static void main(String[] args) {
+
+        int i = 10;
+        int j = 7;
+
+        //Before swapping variable
+        System.out.println(i);
+        System.out.println(j);
+
+       //After swapping variable
+
+        int temp = i;   //10
+        i = j;          //i=7
+        j = temp;       //j=10
+
+       //After swapping variable
+
+        System.out.println(i);
+        System.out.println(j);
+
+
+    }
+
+}
