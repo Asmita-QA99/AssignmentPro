ed42ba5f10806011b401ed7549e3f6031c3ed7b5
Asmita-QA99 <asmita.bramhankar99@gmail.com>
2025-07-03 09:40:35 +0530

    Update DayOfWeekClass.java

--- src/dayofweek/DayOfWeekClass.java
+++ src/dayofweek/DayOfWeekClass.java
@@ -8,35 +8,30 @@ public class DayOfWeekClass {
 
         System.out.print("Enter the number from 1 to 7 to get the day of the week: ");
         int day = sc.nextInt();
-        switch (day){
-            case 1:
-                System.out.print("1 - Sunday");
-                break;
-            case 2:
-                System.out.print("2 - Monday");
-                break;
 
-            case 3:
-                System.out.print("3 - Tuesday");
-                break;
-            case 4:
-                System.out.print("4 - Wednesday");
-                break;
-            case 5:
-                System.out.print("5 - Thursday");
-                break;
-            case 6:
-                System.out.print("6 - Friday");
-                break;
-            case 7:
-                System.out.print("7 - Saturday");
-                break;
-
-            default:
-                System.out.println("Invalid input!");
-
+        if(day == 1) {
+            System.out.println(" 1 - Sunday");
+        }
+        else if(day == 2) {
+            System.out.println(" 2 - Monday");
+        }
+        else if(day == 3) {
+            System.out.println(" 3 - Tuesday");
+        }
+        else if(day == 4) {
+            System.out.println(" 4 - Wednesday");
+        }
+        else if(day == 5) {
+            System.out.println(" 5 - Thursday");
+        }
+        else if(day == 6) {
+            System.out.println(" 6 - Friday");
+        }
+        else if(day == 7) {
+            System.out.println(" 7 - Saturday");
+        }
 
         }
 
     }
-}
+
